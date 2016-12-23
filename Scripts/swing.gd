4f988802d8947c90e1c94a0dfd42c31cc7bdf460
HikaruAikawa <crivtox@gmail.com>
2016-12-23 14:07:54 +0100

    Update swing.gd

--- Scripts/swing.gd
+++ Scripts/swing.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+#DEFINITION OF CONSTANTS
+
+const ST_IDLE = 0
+const ST_MOVING = 1
+const ST_HURT = 2
+const DR_UP = 0
+const DR_LEFT = 1
+const DR_DOWN = 2
+const DR_RIGHT = 3
+
+#DEFINITION OF VARIABLES
+
 #The user of this swing
 var user
 #Timers for the swing
@@ -25,6 +37,14 @@ func _ready():
 	var shape = RectangleShape2D.new()
 	shape.set_extents(Vector2(size/sqrt(2),10))
 	hitbox.add_shape(shape,Matrix32(45,Vector2(size/2,-size/2)))
+	#Gets positioned according to the user's current direction
+	if (user.get_direction() == DR_RIGHT):
+		rotate(-PI/2)
+	elif (user.get_direction() == DR_LEFT):
+		rotate(PI/2)
+	elif (user.get_direction() == DR_DOWN):
+		rotate(PI)
+	set_process(true)
 
 func initialize(usr,tex,sw_time,ang,sz):
 	user = usr
