afa097fb7260a189d6910726563ac37f9cfd831d
HikaruAikawa <crivtox@gmail.com>
2017-01-03 12:02:54 +0100

    Script update 5

--- Scripts/Skills/skill_1.gd
+++ Scripts/Skills/skill_1.gd
@@ -1,5 +1,10 @@
 extends "res://Scripts/skill.gd"
 
+const ST_IDLE = 0
+const ST_MOVING = 1
+const ST_HURT = 2
+const ST_SKILL = 3
+
 var swing_scn = preload("res://Scenes/Swing.tscn")
 var swing
 var texture
@@ -13,7 +18,8 @@ func _process(delta):
 	pass
 
 func effect():
+	user.set_using_skill(0.2)
 	swing = swing_scn.instance()
-	swing.initialize(user,texture,2,90,24)
+	swing.initialize(user,texture,0.2,PI/2,24)
 	add_child(swing)
 	swing.set_owner(self)
