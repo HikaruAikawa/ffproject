201b5f93d45e565a9e14d1d29af6040862d6cf1f
HikaruAikawa <crivtox@gmail.com>
2017-01-10 13:03:35 +0100

    Input menu can now change input keys

--- Scripts/Control/input_menu.gd
+++ Scripts/Control/input_menu.gd
@@ -1,6 +1,9 @@
 extends VBoxContainer
 
 var button_list
+var listening
+var listening_action
+var message_label
 
 func _ready():
 	button_list = []
@@ -8,6 +11,13 @@ func _ready():
 	var label
 	var button
 	var event_list
+	
+	message_label = Label.new()
+	add_child(message_label)
+	message_label.set_owner(self)
+	message_label.set_align(ALIGN_CENTER)
+	message_label.set_text("Press a button to change the input key")
+	
 	var actions = InputMap.get_actions()
 	actions.sort()
 	for action in actions:
@@ -26,9 +36,38 @@ func _ready():
 			button = Button.new()
 			container.add_child(button)
 			button.set_owner(container)
+			button.set_name("button_"+action)
+			button_list.append(button)
 			
 			event_list = InputMap.get_action_list(action)
 			for event in event_list:
 				if (event.type == InputEvent.KEY):
 					button.set_text(OS.get_scancode_string(event.scancode))
-	
+			
+			button.connect("pressed",self,"_action_button_pressed",[action])
+	set_process_input(true)
+	listening = false
+
+func _action_button_pressed(action):
+	if (!listening):
+		message_label.set_text("Press any key")
+		listening = true
+		listening_action = action
+
+func _input(event):
+	if (listening):
+		if (event.type == InputEvent.KEY):
+			for other_event in InputMap.get_action_list(listening_action):
+				if (other_event.type == InputEvent.KEY):
+					InputMap.action_erase_event(listening_action,other_event)
+			InputMap.action_add_event(listening_action,event)
+			listening = false
+			message_label.set_text("Press a button to change the input key")
+			update_buttons()
+
+func update_buttons():
+	for button in button_list:
+		var action = button.get_name().right(7)
+		for event in InputMap.get_action_list(action):
+			if (event.type == InputEvent.KEY):
+				button.set_text(OS.get_scancode_string(event.scancode))
