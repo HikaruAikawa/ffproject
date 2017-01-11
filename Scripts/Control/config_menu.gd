0a4e799b683edcf78dea8d3b71ad8aec195fd0a6
HikaruAikawa <crivtox@gmail.com>
2017-01-11 09:19:20 +0100

    Config now saves to a file

--- Scripts/Control/config_menu.gd
+++ Scripts/Control/config_menu.gd
@@ -1,6 +1,25 @@
 extends VBoxContainer
 
+var message_label
+var save_button
+
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	message_label = find_node("MessageLabel")
+	save_button = find_node("SaveButton")
+	save_button.connect("pressed",self,"_save_button_pressed")
+
+func _save_button_pressed():
+	var file = ConfigFile.new()
+	
+	file.set_value("General", "HRes", get_text_int("HResC"))
+	file.set_value("General", "VRes", get_text_int("VResC"))
+	
+	var config = get_node("/root/config")
+	if (config.save_config(file)): message_label.set_text("Configuration saved successfully")
+	else: message_label.set_text("Error saving configuration")
+
+func get_text(node):
+	return find_node(node).find_node("LineEdit").get_text()
+
+func get_text_int(node):
+	return int(get_text(node))
--- Scripts/config.gd
+++ Scripts/config.gd
@@ -1,11 +1,16 @@
 extends Node
 
+#Paths to find the files
 var input_path
+var config_path
 
 func _ready():
 	input_path = "res://input.cfg"
+	config_path = "res://config.cfg"
 	load_inputs()
 
+#Instead of receiving the file as an argument, it saves directly from the input map
+#(Only the actions that start with gm_ (game) or db_ (debug))
 func save_inputs():
 	var input_file = ConfigFile.new()
 	for action in InputMap.get_actions():
@@ -20,6 +25,7 @@ func save_inputs():
 	if (err == OK): return true
 	else: return false
 
+#Loads the data from the file directly into the input map
 func load_inputs():
 	var key_code
 	var new_event
@@ -28,9 +34,11 @@ func load_inputs():
 	if (err == OK):
 		for action in InputMap.get_actions():
 			if (action.begins_with("gm_") || action.begins_with("db_")):
+				#For each action, deletes all previous keyboard events
 				for event in InputMap.get_action_list(action):
 					if (event.type == InputEvent.KEY):
 						InputMap.action_erase_event(action,event)
+				#Then saves the keyboard event found in the file
 				if (action.find("p1") != -1):
 					key_code = input_file.get_value("p1",action)
 				elif (action.find("p2") != -1):
@@ -40,4 +48,10 @@ func load_inputs():
 				new_event.scancode = key_code
 				InputMap.action_add_event(action,new_event)
 		return true
+	else: return false
+
+#Takes the file passed as an argument and saves it as config.cfg
+func save_config(file):
+	var err = file.save(config_path)
+	if (err == OK): return true
 	else: return false
