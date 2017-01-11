88feb038b03b0f2688a994fd7306cac03a521de3
HikaruAikawa <crivtox@gmail.com>
2017-01-11 13:38:23 +0100

    Fixed being able to teleport out of the map

--- Scripts/Skills/skill_0.gd
+++ Scripts/Skills/skill_0.gd
@@ -1,6 +1,7 @@
 extends "res://Scripts/skill.gd"
 
 var target
+var game_area
 
 #CLASS METHODS
 
@@ -9,7 +10,7 @@ static func get_cooldown(): return 2
 
 #OTHER METHODS
 
-func _init():
+func _ready():
 	mp_cost = 20
 	cooldown = 2
 	
@@ -21,12 +22,14 @@ func _init():
 	rect.set_extents(Vector2(15,15))
 	target.add_shape(rect)
 	target.set_shape_as_trigger(0,true)
+	
+	game_area = get_node("/root/Main/Map/GameArea")
 
 func _process(delta):
 	target.set_pos(96*user.get_forward())
 
 func effect():
-	if(target.get_overlapping_bodies().empty()):
+	if(target.get_overlapping_bodies().empty() && target.overlaps_area(game_area)):
 		user.set_global_pos(target.get_global_pos())
 		return true
 	else:
