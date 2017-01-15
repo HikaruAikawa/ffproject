3d0eae879d8ccbac83d9b9ae6e2a1d15426fbecb
HikaruAikawa <crivtox@gmail.com>
2017-01-15 18:55:56 +0100

    Fixed teleport not working correctly

--- Scripts/Skills/skill_1.gd
+++ Scripts/Skills/skill_1.gd
@@ -25,6 +25,7 @@ func _ready():
 	target.set_shape_as_trigger(0,true)
 	
 	target.set_layer_mask_bit(cons.LYB_DEFAULT,false)
+	target.set_collision_mask_bit(cons.LYB_WALLS,true)
 	target.set_collision_mask_bit(cons.LYB_GAME_AREA,true)
 	
 	game_area = get_node("/root/Main/Map/GameArea")
