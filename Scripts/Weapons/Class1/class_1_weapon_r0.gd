61e06db5d25dcc25719cd03059e1da04e7430abd
HikaruAikawa <crivtox@gmail.com>
2017-01-18 17:09:24 +0100

    Added a new skill

--- Scripts/Weapons/Class1/class_1_weapon_r0.gd
+++ Scripts/Weapons/Class1/class_1_weapon_r0.gd
@@ -2,4 +2,4 @@ extends "res://Scripts/weapon.gd"
 
 static func get_name(): return "Magic Sword"
 static func get_texture(): return load("res://Textures/Weapons/sword_0.tex")
-static func get_skill_ids(): return [0,5]
+static func get_skill_ids(): return [0,7]
