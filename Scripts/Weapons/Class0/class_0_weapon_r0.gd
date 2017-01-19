d7af9364b6579b1f0077fdf1ba785b5cb31a2532
HikaruAikawa <crivtox@gmail.com>
2017-01-19 16:38:16 +0100

    Minor changes

--- Scripts/Weapons/Class0/class_0_weapon_r0.gd
+++ Scripts/Weapons/Class0/class_0_weapon_r0.gd
@@ -2,8 +2,4 @@ extends "res://Scripts/weapon.gd"
 
 static func get_name(): return "Sword"
 static func get_texture(): return load("res://Textures/Weapons/sword_0.tex")
-<<<<<<< HEAD
-static func get_skill_ids(): return [0,6]
-=======
 static func get_skill_ids(): return [0,3,2]
->>>>>>> control
